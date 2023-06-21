1cfabb291c57b82e48b4999b42eaefc881119fa9
Fahim-Khan-P <fahim.tanvir2021@gmail.com>
2023-06-21 23:29:14 +0430

    Created delete comment button!

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,7 +3,7 @@ class Ability
 
   def initialize(user)
     can :manage, :all if user.role == 'admin'
-
+    can :read, :all
     can :manage, Post, author_id: user.id
     can :manage, Comment, author_id: user.id
   end
